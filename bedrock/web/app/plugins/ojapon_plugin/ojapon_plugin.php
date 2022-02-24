22cb11831e2ad8869581d0907f101fa9b88a94ec
AurelieCuignet <aurelie.cuignet@gmail.com>
2022-02-24 23:38:59 +0100

    Created ojapon_plugin to add custom features, added user API registration script

---
+++ bedrock/web/app/plugins/ojapon_plugin/ojapon_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * O'Japon plugin for WordPress
+ *
+ * Plugin Name:  Voyage O'Japon
+ * Description:  A collection of useful features for our collaborative website around trips to Japan
+ * Version:      0.1
+ * Author:       Aurélie Cuignet
+ */
+
+include plugin_dir_path(__FILE__) . 'scripts/registration.php';
